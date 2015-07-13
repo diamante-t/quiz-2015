54f385ec81e0708b7eaf059c16e324bafc057853
Nelson <sdcodificado@gmail.com>
2015-07-13 16:57:23 -0300

    Se agrega autor

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quizz' });
 });
 
+//preguntas y respuestas
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+//author
+router.get("/author",function (req,res) {
+  res.render("author", {author: "Nelson"});
+});
 
 module.exports = router;
